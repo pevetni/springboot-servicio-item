25fef18d12e1f92fae69486a331faf3bda1d1618
Carlos Rodriguez <crseijasnohe@gmail.com>
2019-11-15 15:40:36 -0300

    configurando spring cloud

--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -2,10 +2,18 @@ package com.formacionbdi.springboot.app.item.controllers;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +29,14 @@ public class ItemController {
 	@Autowired
 	@Qualifier("serviceRestTemplate")
 	private IItemService itemService;
+	
+	@Value("${configuracion.texto}")
+	private String texto;
+	
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+	
+	@Autowired
+	private Environment env;
 
 	@GetMapping("/listar")
 	public List<Item> listar(){
@@ -32,6 +48,24 @@ public class ItemController {
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
 	}
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+		log.info(texto);
+		
+		Map<String, String> json = new HashMap();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		
+		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("mail", env.getProperty("configuracion.autor.correo"));
+		}
+		
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK); 
+	}
+	
+	
 	
 	public Item metodoAlternativo(Long id, Integer Cantidad) {
 		Producto prod = new Producto(1L,"DEFAULT", new BigDecimal(0) , new Date(), 0);
